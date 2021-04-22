e7fbaba33cb1fab78c254e439a3ea805e00d0499
Shivam Dwivedi <shivamd018@gmail.com>
2021-04-22 17:27:21 +0530

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+# 1,2,3,4,5,6,7,8,9,11,22,33,44,55,66,77,88,99,101.....
+
+string=input(("Enter a letter:"))  
+if(string==string[::-1]):  
+      print("The letter is a palindrome")  
+else:  
+      print("The letter is not a palindrome")  
