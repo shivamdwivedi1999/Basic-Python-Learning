093ec492df6078921e9ceb949095c4994e095ac3
Shivam Dwivedi <shivamd018@gmail.com>
2021-04-22 10:31:52 +0530

    prime

---
+++ prime.py
@@ -0,0 +1,11 @@
+A=int(input("enter the no  : "))
+
+if A>1:
+    for i in range(2,A):
+        if (A%i)==0:
+            print("it is not prime number")
+            break
+    else:
+        print("no. is prime no")
+else:
+    print("it is not prime no")
