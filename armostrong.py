03ce3f5640ad8a400ad108186ddd4fb99c6c46f2
Shivam Dwivedi <shivamd018@gmail.com>
2021-04-26 22:29:00 +0530

    armostrong

---
+++ armostrong.py
@@ -0,0 +1,14 @@
+#407= 4^3+0^3+7^3
+#345= 3^3+4^3+5^3
+
+num = int(input("Enter a number: "))
+sum = 0
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
